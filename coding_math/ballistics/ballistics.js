6af1f68051ceb86286d74e201e88d4ae6c2fe7af
Jouke van Leeuwen <joukevanleeuwen@gmail.com>
2021-03-07 14:09:50 +0100

    start with explosion effect in ballistics game

--- coding_math/ballistics/ballistics.js
+++ coding_math/ballistics/ballistics.js
@@ -13,7 +13,30 @@ window.onload = function() {
         forceAngle = 0,
         forceSpeed = 0.1,
 		rawForce = 0,
-		target = {};
+		target = {},
+		counter = 0,
+		explosionFired = false,
+		colorArray = [
+				'#198DBF',
+				'#115E80',
+				'#22BCFF',
+				'#082F40',
+				'#1EAAE6',
+				'#6206C2',
+				'#AC5CFF',
+				'#9F42FF',
+				'#9F1FFF',
+				'#7EFF47',
+			],
+		particles = [],
+		numParticles = 500;
+
+	// fill the particles array
+	for (let i = 0; i < numParticles; i++) {
+		particles.push(particle.create(width / 2, height / 2, Math.random() * 8 + 1, Math.random() * Math.PI * 2, 0.02))
+
+	};
+
 
 	cannonball.radius = 7;
 
@@ -25,6 +48,7 @@ window.onload = function() {
 		target.x = utils.randomRange(200, width);
 		target.y = utils.randomRange(100, height - 80);
 		target.radius = utils.randomRange(20, 80);
+
 	}
 
 
@@ -72,14 +96,39 @@ window.onload = function() {
 		if(cannonball.y > height) {
             isShooting = false;
 		}
+
+		if(explosionFired == true) {
+			for (let i = 0; i < numParticles; i++) {
+				let p = particles[i];
+
+				p.update();
+
+				context.beginPath();
+				context.arc(p.x, p.y, utils.lerp(p.getSpeed(), 0.0001, 1), Math.PI * 2, false);
+				context.fillStyle = colorArray[2]
+				context.fill();
+
+			}
+			counter++;
+			console.log(counter);
+			
+			
+		}
+		
+
+		
 		requestAnimationFrame(update);
 		
+		
+		
 	}
-
+	
+	console.log("counter: " + counter);
+	
+	
 	function checkTarget() {
 		if(utils.circleCollision(target, cannonball)) {
-			// create amazing explosion effect before setting new target
-			
+			explosionFired = true;
 			setTarget();
 		}
 	}
@@ -112,7 +161,7 @@ window.onload = function() {
 
         context.fillStyle = "#666";
         context.fillRect(10, height - 10, 20, utils.map(rawForce, -1, 1, 0, -100));
-        console.log(rawForce);
+        // console.log(rawForce);
         context.fillStyle = "#000";
 		context.beginPath();
 		context.arc(gun.x, gun.y, 24, 0, Math.PI * 2, false);
@@ -135,6 +184,8 @@ window.onload = function() {
 		context.beginPath();
 		context.arc(target.x, target.y, target.radius, 0, Math.PI * 2, false);
 		context.fill();
+
+		
 
 	}
 
